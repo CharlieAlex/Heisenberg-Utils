2ba932fa3eac874d88d17d4d2da3013a9c9ab133
羅偉駿 <weichun.lo@K2811.local>
2025-09-06 07:52:57 +0800

    調整 init 中的 sa 路徑，version調整成0.1.3

--- src/heisenberg_utils/__init__.py
+++ src/heisenberg_utils/__init__.py
@@ -3,7 +3,12 @@ from pathlib import Path
 
 from loguru import logger
 
-credentials_path = Path(os.getenv("GOOGLE_APPLICATION_CREDENTIALS", ""))
+MAIN_PATH = Path(os.getenv("MAIN_PATH", ""))
+if not os.getenv("MAIN_PATH"):
+    logger.warning("No MAIN_PATH found, please add MAIN_PATH to your environment variables")
+
+credentials_path = MAIN_PATH / Path(os.getenv("GOOGLE_APPLICATION_CREDENTIALS", "sa/sa.json"))
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str(credentials_path)
 if credentials_path.is_file() is False:
     logger.error(f"Google application credentials file not found: {credentials_path}")
     raise FileNotFoundError("Please add GOOGLE_APPLICATION_CREDENTIALS to your environment variables.")
